bf47ebeabbc52dff55f8ef495fd07ef5ad3afba0
Bear <timfanda35@gmail.com>
2014-05-31 21:31:42 +0800

    Initial Commit

---
+++ HelloBlock/bearAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  bearAppDelegate.m
+//  HelloBlock
+//
+//  Created by Bear on 2014/5/31.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import "bearAppDelegate.h"
+
+@implementation bearAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ HelloBlockTests/HelloBlockTests.m
@@ -0,0 +1,34 @@
+//
+//  HelloBlockTests.m
+//  HelloBlockTests
+//
+//  Created by Bear on 2014/5/31.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloBlockTests : XCTestCase
+
+@end
+
+@implementation HelloBlockTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
